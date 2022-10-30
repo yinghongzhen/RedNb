805829c7960dca2fc477a894029c1eb844b7cb44
应红振 <442962355@qq.com>
2022-10-30 09:22:01 +0800

    修改实体类

--- src/services/auth/RedNb.Auth.Domain/Roles/Role.cs
+++ src/services/auth/RedNb.Auth.Domain/Roles/Role.cs
@@ -1,50 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Text;
-using RedNb.Auth.Domain.Shared.Enums;
-using RedNb.Auth.Domain.Tenants;
-using RedNb.Core.Domain;
-using RedNb.Core.Domain.Audit;
+﻿using RedNb.Auth.Domain.Tenants;
 
-namespace RedNb.Auth.Domain.Roles
+namespace RedNb.Auth.Domain.Roles;
+
+/// <summary>
+/// 角色实体类
+/// </summary>
+[Table("Role")]
+public class Role : AuditFullEntity, IHasTenant
 {
     /// <summary>
-    /// 角色实体类
+    /// 名称
     /// </summary>
-    [Table("Role")]
-    public class Role : AuditFullEntity, IHasTenant
-    {
-        /// <summary>
-        /// 名称
-        /// </summary>
-        [Required]
-        [MaxLength(100)]
-        public string Name { get; set; }
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; }
 
-        /// <summary>
-        /// 编码
-        /// </summary>
-        [Required]
-        [MaxLength(20)]
-        public string Key { get; set; }
+    /// <summary>
+    /// 编码
+    /// </summary>
+    [Required]
+    [MaxLength(20)]
+    public string Key { get; set; }
 
-        /// <summary>
-        /// 排序号
-        /// </summary>
-        [Required]
-        public decimal Sort { get; set; }
+    /// <summary>
+    /// 排序号
+    /// </summary>
+    [Required]
+    public decimal Sort { get; set; }
 
-        [Required]
-        public bool IsActive { get; set; }
+    [Required]
+    public bool IsActive { get; set; }
 
-        [Required]
-        public bool IsDeleted { get; set; }
+    [Required]
+    public bool IsDeleted { get; set; }
 
-        [Required]
-        public long TenantId { get; set; }
+    [Required]
+    public long TenantId { get; set; }
 
-        public virtual Tenant Tenant { get; set; }
-    }
+    public virtual Tenant Tenant { get; set; }
 }
