5929fb9bd781f8821b684651fb886f7187a04a89
应红振 <442962355@qq.com>
2022-11-10 16:15:35 +0800

    完善结构

--- src/gateways/web/RedNb.WebGateway.Domain.Shared/GlobalUsing.cs
+++ src/gateways/web/RedNb.WebGateway.Domain.Shared/GlobalUsing.cs
@@ -1,5 +1,4 @@
-﻿global using Volo.Abp.Application.Services;
-global using Volo.Abp.DependencyInjection;
+﻿global using Volo.Abp.DependencyInjection;
 global using Volo.Abp.Modularity;
 global using Volo.Abp.ObjectExtending;
 global using Volo.Abp.Threading;
--- src/gateways/web/RedNb.WebGateway.Domain.Shared/WebGatewayDomainSharedModule.cs
+++ src/gateways/web/RedNb.WebGateway.Domain.Shared/WebGatewayDomainSharedModule.cs
@@ -1,36 +1,17 @@
 ﻿using RedNb.WebGateway.Localization;
-using Volo.Abp.AuditLogging;
-using Volo.Abp.BackgroundJobs;
-using Volo.Abp.FeatureManagement;
-using Volo.Abp.Identity;
 using Volo.Abp.Localization;
 using Volo.Abp.Localization.ExceptionHandling;
-using Volo.Abp.Modularity;
-using Volo.Abp.OpenIddict;
-using Volo.Abp.PermissionManagement;
-using Volo.Abp.SettingManagement;
-using Volo.Abp.TenantManagement;
 using Volo.Abp.Validation.Localization;
 using Volo.Abp.VirtualFileSystem;
 
 namespace RedNb.WebGateway;
 
-[DependsOn(
-    typeof(AbpAuditLoggingDomainSharedModule),
-    typeof(AbpBackgroundJobsDomainSharedModule),
-    typeof(AbpFeatureManagementDomainSharedModule),
-    typeof(AbpIdentityDomainSharedModule),
-    typeof(AbpOpenIddictDomainSharedModule),
-    typeof(AbpPermissionManagementDomainSharedModule),
-    typeof(AbpSettingManagementDomainSharedModule),
-    typeof(AbpTenantManagementDomainSharedModule)    
-    )]
+[DependsOn()]
 public class WebGatewayDomainSharedModule : AbpModule
 {
     public override void PreConfigureServices(ServiceConfigurationContext context)
     {
-        WebGatewayGlobalFeatureConfigurator.Configure();
-        WebGatewayModuleExtensionConfigurator.Configure();
+        
     }
 
     public override void ConfigureServices(ServiceConfigurationContext context)
