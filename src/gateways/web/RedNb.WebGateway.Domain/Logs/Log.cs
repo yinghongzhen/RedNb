8c1c79952233b13d06c0bf0374ddfe7c248c8b02
应红振 <442962355@qq.com>
2022-12-07 19:12:00 +0800

    增加日志

---
+++ src/gateways/web/RedNb.WebGateway.Domain/Logs/Log.cs
@@ -0,0 +1,13 @@
+﻿using RedNb.Core.Domain;
+
+namespace RedNb.WebGateway.Domain.Routes;
+
+[Table("Log")]
+public class Log : EntityBase
+{
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; }
+
+    public virtual Route Route { get; set; }
+}
