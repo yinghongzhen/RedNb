17502a288e6fd8d492a05d3a88afdce540b5fc68
应红振 <442962355@qq.com>
2022-12-12 10:30:50 +0800

    文件清理

---
+++ src/services/aggregates/gateway/RedNb.Gateway.Domain.Shared/Enums/ELogType.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace RedNb.Gateway.Domain.Shared.Enums;
+
+/// <summary>
+/// 日志类型
+/// </summary>
+[Description("日志类型")]
+public enum ELogType
+{
+    /// <summary>
+    /// 登录登出
+    /// </summary>
+    [Description("登录登出")]
+    LoginOrLogout = 0,
+
+    /// <summary>
+    /// 增删改查
+    /// </summary>
+    [Description("增删改查")]
+    Crud = 1
+}
